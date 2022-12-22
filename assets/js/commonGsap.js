be514526bdc9dc5e5a28f1bca3d46cfd74008ced
KDB6 <ipkg7210@gmail.com>
2022-12-22 16:06:17 +0900

    12 22
    12 22

--- assets/js/commonGsap.js
+++ assets/js/commonGsap.js
@@ -257,6 +257,44 @@ h1.from('.rotate', {
 
 
 
+// img hover gsap
+let menuItem = document.querySelectorAll(".menu__item-text");
+let menuImage = document.querySelectorAll(".menu__item-image");
+
+// adding eventListeners to all the menuItems.
+for (let i = 0; i < 6; i++) {
+    //   image reveal animation
+    const animation = gsap.to(menuImage[i], {
+        opacity: 1,
+        duration: 0.2,
+        scale: 1,
+        ease: "ease-in-out"
+    });
+
+    menuItem[i].addEventListener("mouseenter", () => animation.play());
+    menuItem[i].addEventListener("mouseleave", () => animation.reverse());
+
+    //   initialization
+    animation.reverse();
+}
+
+//   to move image along with cursor
+function moveText(e) {
+    gsap.to([...menuImage], {
+        css: {
+        left: e.pageX + 50,
+        top: e.pageY,
+        },
+        duration: 0.3,
+    });
+}
+
+menuItem.forEach((el) => {
+    el.addEventListener("mousemove", moveText);
+});
+
+
+
 // section delay
 let a1 = gsap.timeline({delay: 3.5});
 let a2 = gsap.timeline({delay: 4.5});
